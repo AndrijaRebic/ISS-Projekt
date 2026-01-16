b745ad6d508a86c34f790cfcc2725f47fc8efc2c
LukaGolub <luka.golub@fer.hr>
2026-01-16 21:35:02 +0100

    Added particle effects to tank shooting

--- Assets/Scripts/Tank/TankShooting.cs
+++ Assets/Scripts/Tank/TankShooting.cs
@@ -6,12 +6,14 @@ public class TankShooting : MonoBehaviour
     [Header("Cannon")]
     public Transform cannonMuzzle;
     public GameObject cannonShellPrefab;
+    public GameObject cannonMuzzleFlashPrefab; // NEW
     public float cannonForce = 1500f;
     public float cannonCooldown = 1.5f;
 
     [Header("Machine Gun")]
     public Transform machineGunMuzzle;
     public GameObject machineGunBulletPrefab;
+    public GameObject lmgMuzzleFlashPrefab;    // NEW
     public float machineGunForce = 800f;
     public float machineGunFireRate = 0.1f;
 
@@ -41,6 +43,16 @@ public class TankShooting : MonoBehaviour
 
             Rigidbody rb = shell.GetComponent<Rigidbody>();
             rb.AddForce(cannonMuzzle.forward * cannonForce);
+
+            // Muzzle flash
+            if (cannonMuzzleFlashPrefab != null)
+            {
+                Instantiate(
+                    cannonMuzzleFlashPrefab,
+                    cannonMuzzle.position,
+                    cannonMuzzle.rotation
+                );
+            }
         }
     }
 
@@ -58,6 +70,19 @@ public class TankShooting : MonoBehaviour
 
             Rigidbody rb = bullet.GetComponent<Rigidbody>();
             rb.AddForce(machineGunMuzzle.forward * machineGunForce);
+
+            // Muzzle flash
+            Quaternion flashRotation = machineGunMuzzle.rotation * Quaternion.Euler(180f, 0f, 0f);
+
+            if (lmgMuzzleFlashPrefab != null)
+            {
+                Instantiate(
+                    lmgMuzzleFlashPrefab,
+                    machineGunMuzzle.position,
+                    flashRotation
+                );
+            }
         }
     }
 }
+
