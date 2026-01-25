a85a260ac92ef84b51dbed7de01ce243f280cb24
Leon Mesic <leon.mesic@fer.hr>
2026-01-25 19:23:38 +0100

    yaw pivot aiming fixed

--- Assets/Scripts/SplitScreenSwitch.cs
+++ Assets/Scripts/SplitScreenSwitch.cs
@@ -14,7 +14,7 @@ public class SplitScreenSwitch : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.T))
+        if (Input.GetKeyDown(KeyCode.C))
         {
             isHorizontal = !isHorizontal;
             UpdateCameraViews();
